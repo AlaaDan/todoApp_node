85a681e54d369455d5f84406d097b55a28d56122
Alaa Aldandachi <58466561+AlaaDan@users.noreply.github.com>
2023-03-19 18:16:07 +0100

    To do API

---
+++ index.js
@@ -0,0 +1,63 @@
+const { request, response } = require("express");
+const express = require("express");
+const app = express();
+const PORT = 8000;
+
+app.use(express.json());
+
+const todosList = [ 
+    {
+        toDo: "Cleadning",
+        id: 1,
+        done: false,
+        createdAt: new Date
+    },
+    {
+        toDo: "Laundry",
+        id: 2,
+        done: false,
+        createdAt: new Date
+    },
+    {
+        toDo: "Cook food",
+        id: 3,
+        done: false,
+        createdAt: new Date
+    },
+
+]
+
+app.get('/api/todo', (request, response)=>{
+    response.json(todosList)
+})
+
+
+app.post('/api/todo', (request, response)=>{
+    const requestedTodo = request.body;
+
+    const result = {
+        success: true
+    }
+
+    if (requestedTodo.hasOwnProperty('toDo') &&
+        requestedTodo.hasOwnProperty('done')){
+            todosList.forEach(todo =>{
+                if (todo.toDo === requestedTodo.toDo){
+                    result.success = false
+                    console.log(`The ${todo} to already exests in the list`)
+                }
+            }) 
+        }
+    if (result.success) {
+        const id = todosList.length + 1
+        const date = new Date
+        requestedTodo.id = id
+        requestedTodo.date = date
+        todosList.push(requestedTodo)
+    }
+    response.json(result)
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Server on port ${PORT} started!`)
+})
